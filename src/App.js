2d39acd8b5c243d0215b5be9d7e035c58487192c
Ben Kehoe <b_man901@hotmail.com>
2024-06-02 13:14:51 +0100

    Remaining and expense total update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from "react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-//Code to import Budget.js
 import Budget from "./components/Budget";
-
-// Add code to import the other components here under
+import Remaining from "./components/Remaining";
+import ExpenseTotal from "./components/ExpenseTotal";
 
 import { AppProvider } from "./context/AppContext";
 const App = () => {
@@ -21,9 +20,21 @@ const App = () => {
               <Budget />
             </div>
           }
-              {/* Add Remaining component here*/}   {" "}
-          {/* Add ExpenseTotal component here */}   {" "}
-          {/* Add ExpenseList component here */}   {" "}
+             {" "}
+          {
+            //Remaining component
+            <div className="col-sm">
+              <Remaining />
+            </div>
+          }
+             {" "}
+          {
+            //ExpenseTotal component
+            <div className="col-sm">
+              <ExpenseTotal />
+            </div>
+          }
+              {/* Add ExpenseList component here */}   {" "}
           {/* Add ExpenseItem component here */}   {" "}
           {/* Add AllocationForm component here under */}
         </div>
